ed20e41c4483c797cf91eaa193bff77646d0a416
AnnaAndreenko <anya.andreenko@mail.ru>
2025-07-22 16:44:19 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,8 +8,15 @@ terraform {
 }
 
 provider "yandex" {
-  service_account_key_file = file("~/authorized_key.json")
+  token     = var.token
   cloud_id  = var.cloud_id
   folder_id = var.folder_id
   zone      = "ru-central1-a"
+}
+
+resource "yandex_storage_bucket" "tf_state" {
+  bucket     = "my-tf-state-bucket"
+  access_key = var.access_key
+  secret_key = var.secret_key
+  acl        = "private"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "cloud_id" {}
+variable "folder_id" {}
+variable "token" {}
+variable "bucket_name" {
+  default = "my-tf-state-bucket"
+}
